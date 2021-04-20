31202c919ae4762e2a0183b84baa7f5952effc6e
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-19 22:18:21 -0300

    update app.ts

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,8 +3,8 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-app.get("/", (request, response) =>
-  response.status(200).json({ message: "Hello NLW#05" })
-);
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello NLW#05" })
+});
 
 export { app };
