bea90d7db7f182820d4cce64ccb6018fc8527bb8
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-19 22:14:02 -0300

    run lint:fix

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,8 +1,10 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (request, response) => response.status(200).json({ message: 'Hello NLW#05' }));
+app.get("/", (request, response) =>
+  response.status(200).json({ message: "Hello NLW#05" })
+);
 
 export { app };
