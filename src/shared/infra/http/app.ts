003fbdf6be6a05fd51a63fc6df40b18f32d31af9
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-20 14:31:54 -0300

    handling with errors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,7 @@ import "@shared/container";
 
 import express, { Request, Response } from "express";
 
+import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 
 createConnection();
@@ -13,6 +14,19 @@ app.use(express.json());
 
 app.get("/", (request: Request, response: Response) => {
   return response.status(200).json({ message: "Hello NLW#05 🚀" });
+});
+
+app.use((err: Error, request: Request, response: Response) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: `Internal server error: \n${err.message}`,
+  });
 });
 
 export { app };
