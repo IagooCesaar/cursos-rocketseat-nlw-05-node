ea26e721a1df83bdeca52bb3429ee909b7ce5600
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-20 13:56:47 -0300

    default importations for app.ts

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,3 +1,7 @@
+import "express-async-errors";
+import "reflect-metadata";
+import "@shared/container";
+
 import express, { Request, Response } from "express";
 
 import createConnection from "@shared/infra/typeorm";
