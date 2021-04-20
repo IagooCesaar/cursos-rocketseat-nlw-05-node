6c847583df0835d08936fdce41e76f2c293a90a4
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-20 15:31:08 -0300

    create CreateSettingsUseCase.spec

---
+++ src/modules/settings/useCases/createSettings/CreateSettingsUseCase.spec.ts
@@ -0,0 +1,22 @@
+import { InMemorySettingsRepository } from "@modules/settings/repositories/in-memory/InMemorySettingsRepository";
+
+import { CreateSettingsUseCase } from "./CreateSettingsUseCase";
+
+let settingsRepository: InMemorySettingsRepository;
+let createSettingsUseCase: CreateSettingsUseCase;
+
+describe("CreateSettingsUseCase", () => {
+  beforeEach(() => {
+    settingsRepository = new InMemorySettingsRepository();
+    createSettingsUseCase = new CreateSettingsUseCase(settingsRepository);
+  });
+
+  it("Should be able to create a new setting", async () => {
+    const setting = {
+      username: "test",
+      chat: true,
+    };
+    const createdSetting = await createSettingsUseCase.execute(setting);
+    expect(createdSetting).toHaveProperty("id");
+  });
+});
