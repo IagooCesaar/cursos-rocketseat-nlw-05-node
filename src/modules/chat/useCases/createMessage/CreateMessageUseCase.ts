3373506dbdfa615743981cdc46c08af8861e4481
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-21 14:26:02 -0300

    validate if user exists

--- src/modules/chat/useCases/createMessage/CreateMessageUseCase.ts
+++ src/modules/chat/useCases/createMessage/CreateMessageUseCase.ts
@@ -1,7 +1,10 @@
 import { inject, injectable } from "tsyringe";
 
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { Message } from "@modules/chat/entities/Message";
 import { IMessageRepository } from "@modules/chat/infra/repositories/IMessagesRepository";
+
+import { CreateMessageError } from "./CreateMessageError";
 
 interface IRequest {
   user_id: string;
@@ -13,10 +16,18 @@ interface IRequest {
 class CreateMessageUseCase {
   constructor(
     @inject("MessagesRepository")
-    private messagesRepository: IMessageRepository
+    private messagesRepository: IMessageRepository,
+
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
   ) {}
 
   async execute({ admin_id, user_id, text }: IRequest): Promise<Message> {
+    const userExists = await this.usersRepository.findById(user_id);
+    if (!userExists) {
+      throw new CreateMessageError.UserNotFound();
+    }
+
     const message = await this.messagesRepository.createMessage({
       user_id,
       text,
