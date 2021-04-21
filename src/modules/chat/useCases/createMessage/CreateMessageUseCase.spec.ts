c44070aefc7e5444f4e2e2c1635cb4af83840511
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-21 14:44:54 -0300

    CreateMessageUseCase.spec

---
+++ src/modules/chat/useCases/createMessage/CreateMessageUseCase.spec.ts
@@ -0,0 +1,55 @@
+import { v4 as uuidV4 } from "uuid";
+
+import { InMemoryUsersRepository } from "@modules/accounts/repositories/in-memory/InMemoryUsersRepository";
+import { InMemoryMessageRepository } from "@modules/chat/infra/repositories/in-memory/InMemoryMessageRepository";
+
+import { CreateMessageError } from "./CreateMessageError";
+import { CreateMessageUseCase } from "./CreateMessageUseCase";
+
+let createMessageUseCase: CreateMessageUseCase;
+let messagesRepository: InMemoryMessageRepository;
+let usersRepository: InMemoryUsersRepository;
+
+describe("CreateMessageUseCase", () => {
+  beforeEach(() => {
+    messagesRepository = new InMemoryMessageRepository();
+    usersRepository = new InMemoryUsersRepository();
+    createMessageUseCase = new CreateMessageUseCase(
+      messagesRepository,
+      usersRepository
+    );
+  });
+
+  it("Should be able to create a message for a user", async () => {
+    const user = await usersRepository.create({ email: "test@test.com" });
+
+    const message = await createMessageUseCase.execute({
+      user_id: user.id,
+      text: "Need Help",
+    });
+
+    expect(message).toHaveProperty("id");
+  });
+
+  it("", async () => {
+    const fakeId = uuidV4();
+
+    await expect(
+      createMessageUseCase.execute({
+        user_id: fakeId,
+        text: "Need help",
+      })
+    ).rejects.toBeInstanceOf(CreateMessageError.UserNotFound);
+  });
+
+  it("Should be able to create a message for a user", async () => {
+    const user = await usersRepository.create({ email: "test@test.com" });
+
+    await expect(
+      createMessageUseCase.execute({
+        user_id: user.id,
+        text: "",
+      })
+    ).rejects.toBeInstanceOf(CreateMessageError.TextMustNotBeEmpty);
+  });
+});
