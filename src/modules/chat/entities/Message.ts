3ccc199dc68e923ec908422183cfb003d4162ca4
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-21 13:33:08 -0300

    Create Message Entity

---
+++ src/modules/chat/entities/Message.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import { User } from "@modules/accounts/entities/Users";
+
+@Entity("messages")
+class Message {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  admin_id: string;
+
+  @Column()
+  user_id: string;
+
+  @JoinColumn({ name: "user_id" })
+  @ManyToOne(() => User)
+  user: User;
+
+  @Column()
+  text: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Message };
---
+++ src/shared/infra/typeorm/migrations/1619016426611-CreateMessages.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateMessages1619016426611 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "messages",
+        columns: [
+          { name: "id", type: "uuid", isPrimary: true },
+          { name: "admin_id", type: "uuid", isNullable: true },
+          { name: "user_id", type: "uuid" },
+          { name: "text", type: "varchar" },
+          { name: "created_at", type: "timestamp", default: "now()" },
+        ],
+        foreignKeys: [
+          {
+            name: "FK_messages_user_id",
+            columnNames: ["user_id"],
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            onUpdate: "NO ACTION",
+            onDelete: "NO ACTION",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("messages");
+  }
+}
