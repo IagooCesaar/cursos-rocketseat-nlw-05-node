0b4c0db85db2a2305efcc9e3ada3729d31fb448e
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-21 15:18:47 -0300

    create ListUserMessagesUseCase

---
+++ src/modules/chat/useCases/listUserMessages/ListUserMessagesUseCase.ts
@@ -0,0 +1,19 @@
+import { inject, injectable } from "tsyringe";
+
+import { Message } from "@modules/chat/entities/Message";
+import { IMessageRepository } from "@modules/chat/repositories/IMessagesRepository";
+
+@injectable()
+class ListUserMessageUseCase {
+  constructor(
+    @inject("MessagesRepository")
+    private messagesRepository: IMessageRepository
+  ) {}
+
+  async execute(user_id: string): Promise<Message[]> {
+    const messages = await this.messagesRepository.findByUserId(user_id);
+    return messages;
+  }
+}
+
+export { ListUserMessageUseCase };
