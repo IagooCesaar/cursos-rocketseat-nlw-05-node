4d6bbd5f3e276f112475b31d6494c40427a8db5a
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-21 10:04:13 -0300

    UsersRepository registerSingleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,16 @@
 import { container } from "tsyringe";
 
+import { UsersRepository } from "@modules/accounts/infra/repositories/UsersRepository";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { SettingsRepository } from "@modules/settings/infra/repositories/SettingsRepository";
 import { ISettingsRepository } from "@modules/settings/repositories/ISettingsRepository";
 
 container.registerSingleton<ISettingsRepository>(
   "SettingsRepository",
   SettingsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
 );
