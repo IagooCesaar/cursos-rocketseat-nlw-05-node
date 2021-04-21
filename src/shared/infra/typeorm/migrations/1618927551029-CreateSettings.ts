0eec986326b784c5f335269caf417a66eb172215
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-21 15:07:17 -0300

    run lint:fix

--- src/shared/infra/typeorm/migrations/1618927551029-CreateSettings.ts
+++ src/shared/infra/typeorm/migrations/1618927551029-CreateSettings.ts
@@ -1,4 +1,4 @@
-import { Column, MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateSettings1618927551029 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
