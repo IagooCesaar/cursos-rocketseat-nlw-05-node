240decdd6a682ec4a58c736ca27b2f942f36db9e
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-21 15:21:55 -0300

    create route to list messages for a user

--- src/shared/infra/http/routes/chat.routes.ts
+++ src/shared/infra/http/routes/chat.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { CreateMessageController } from "@modules/chat/useCases/createMessage/CreateMessageController";
+import { ListUserMessagesController } from "@modules/chat/useCases/listUserMessages/ListUserMessagesController";
 
 const chatRoutes = Router();
 
 const createMessageController = new CreateMessageController();
+const listUserMessagesController = new ListUserMessagesController();
 
 chatRoutes.post("/", createMessageController.handle);
+chatRoutes.get("/:user_id", listUserMessagesController.handle);
 
 export { chatRoutes };
