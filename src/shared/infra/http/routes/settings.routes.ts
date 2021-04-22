e829b7973499ae3e10bda3e5adebf50afac65873
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-22 16:01:48 -0300

    path route for updateSettings

--- src/shared/infra/http/routes/settings.routes.ts
+++ src/shared/infra/http/routes/settings.routes.ts
@@ -2,13 +2,16 @@ import { Router } from "express";
 
 import { CreateSettingsController } from "@modules/settings/useCases/createSettings/CreateSettingsController";
 import { GetSettingsController } from "@modules/settings/useCases/getSettingsForUsername/GetSettingsController";
+import { UpdateSettingsController } from "@modules/settings/useCases/updateSettings/UpdateSettingsController";
 
 const settingsRoutes = Router();
 
 const createSettingsController = new CreateSettingsController();
 const getSettingsController = new GetSettingsController();
+const updateSettingsController = new UpdateSettingsController();
 
 settingsRoutes.post("/", createSettingsController.handle);
 settingsRoutes.get("/:username", getSettingsController.handle);
+settingsRoutes.patch("/:username", updateSettingsController.handle);
 
 export { settingsRoutes };
