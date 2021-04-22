f13374452202ed777dace15a62c18c419acb12f4
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-22 14:42:06 -0300

    changes to connect with socket.io server

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,36 @@
-document.querySelector("#start_chat").addEventListener("click", (event) => {});
+const chatForm = {
+  socket: null,
+
+  chatHelp: document.querySelector("#chat_help"),
+  chatInSupport: document.querySelector("#chat_in_support"),
+
+  textArea: document.getElementById("txt_help"),
+  email: document.getElementById("email"),
+
+  changeToHistory() {
+    chatForm.chatHelp.style.display = "none";
+    chatForm.chatInSupport.style.display = "block";
+  },
+
+  init() {
+    console.log("Iniciou o chat");
+    chatForm.socket = io();
+
+    const params = {
+      text: chatForm.textArea.value,
+      email: chatForm.email.value,
+    };
+
+    chatForm.socket.on("connect", () => {
+      chatForm.socket.emit("client_first_access", params, (call, err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(call);
+        }
+      });
+    });
+
+    chatForm.changeToHistory();
+  },
+};
