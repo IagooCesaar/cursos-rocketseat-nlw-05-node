48371df72b209ba5af6bde54c604b24d11e0cd3c
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-22 13:49:49 -0300

    ConnectionsRepository registerSingleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,7 +2,9 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { ConnectionsRepository } from "@modules/chat/infra/repositories/ConnectionsRepository";
 import { MessagesRepository } from "@modules/chat/infra/repositories/MessagesRepository";
+import { IConnectionsRepository } from "@modules/chat/repositories/IConnectionsRepository";
 import { IMessageRepository } from "@modules/chat/repositories/IMessagesRepository";
 import { SettingsRepository } from "@modules/settings/infra/repositories/SettingsRepository";
 import { ISettingsRepository } from "@modules/settings/repositories/ISettingsRepository";
@@ -20,4 +22,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IMessageRepository>(
   "MessagesRepository",
   MessagesRepository
+);
+
+container.registerSingleton<IConnectionsRepository>(
+  "ConnectionsRepository",
+  ConnectionsRepository
 );
