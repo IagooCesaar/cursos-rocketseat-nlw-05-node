d97ce0a9f6e2a1949d5c3badb5c7fe0b14b95b88
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-23 10:33:45 -0300

    rendering user's messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,8 +1,67 @@
-const chatForm = {
+const socketHandler = {
   socket: null,
+  chatForm: null,
 
+  init(chatForm) {
+    socketHandler.chatForm = chatForm;
+    socketHandler.socket = io();
+    socketHandler.socket.on("connect", socketHandler.onConnect);
+
+    return true;
+  },
+
+  onConnect() {
+    const params = {
+      text: socketHandler.chatForm.textArea.value,
+      email: socketHandler.chatForm.email.value,
+    };
+
+    socketHandler.socket.emit(
+      "client_first_access",
+      params,
+      (callback, err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(callback);
+        }
+      }
+    );
+
+    socketHandler.socket.on(
+      "client_list_all_messages",
+      socketHandler.onGetAllMessages
+    );
+  },
+
+  onGetAllMessages(messages) {
+    const templateClient = socketHandler.chatForm.templateClient.innerHTML;
+    const templateAdmin = socketHandler.chatForm.templateAdmin.innerHTML;
+
+    messages.forEach((message) => {
+      if (!message.admin_id) {
+        const rendered = Mustache.render(templateClient, {
+          message: message.text,
+          email: message.user.email,
+        });
+
+        document.getElementById("messages").innerHTML += rendered;
+      } else {
+        const rendered = Mustache.render(templateAdmin, {
+          message_admin: message.text,
+        });
+        document.getElementById("messages").innerHTML += rendered;
+      }
+    });
+  },
+};
+
+const chatForm = {
   chatHelp: document.querySelector("#chat_help"),
   chatInSupport: document.querySelector("#chat_in_support"),
+
+  templateClient: document.getElementById("message-user-template"),
+  templateAdmin: document.getElementById("admin-template"),
 
   textArea: document.getElementById("txt_help"),
   email: document.getElementById("email"),
@@ -14,23 +73,8 @@ const chatForm = {
 
   init() {
     console.log("Iniciou o chat");
-    chatForm.socket = io();
-
-    const params = {
-      text: chatForm.textArea.value,
-      email: chatForm.email.value,
-    };
-
-    chatForm.socket.on("connect", () => {
-      chatForm.socket.emit("client_first_access", params, (call, err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log(call);
-        }
-      });
-    });
-
-    chatForm.changeToHistory();
+    if (socketHandler.init(chatForm)) {
+      chatForm.changeToHistory();
+    }
   },
 };
