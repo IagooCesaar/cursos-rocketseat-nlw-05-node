3ded1cb8ce66bbca026cfefd422a811ed5033988
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-04-23 13:49:57 -0300

    rendering list of users

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,21 @@
+const chatForm = {
+  usersList: document.getElementById("list_users"),
+  userListTemplate: document.getElementById("template"),
+
+  renderUsersList(connections) {
+    chatForm.usersList.innerHTML = "";
+
+    connections.forEach((connection) => {
+      const rendered = Mustache.render(chatForm.userListTemplate.innerHTML, {
+        id: connection.socket_id,
+        email: connection.user.email,
+      });
+
+      chatForm.usersList.innerHTML += rendered;
+    });
+  },
+};
+
 const socketHandler = {
   socket: null,
 
@@ -10,7 +28,7 @@ const socketHandler = {
   },
 
   onGetListUsers(connections) {
-    console.log("here", connections);
+    chatForm.renderUsersList(connections);
   },
 };
 
